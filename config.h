29d4f7406754cc90d0471bcec1b7da0f865ba18a
nytouu <capitaine7312@gmail.com>
2021-05-12 19:17:06 +0200

    focusonclick patch

--- config.h
+++ config.h
@@ -11,6 +11,7 @@ static const unsigned int gappov    = 16;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static const int vertpad 			= 6; 		/* vertical padding of bar */
 static const int sidepad 			= 6; 		/* horizontal padding of bar */
 static const int user_bh            = 24;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
