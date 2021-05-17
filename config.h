8b3a03ff8765dc43cd90009414eb08897757f367
nytouu <capitaine7312@gmail.com>
2021-05-17 03:36:03 +0200

    floating nmtui for network manager

--- config.h
+++ config.h
@@ -57,6 +57,7 @@ static const Rule rules[] = {
 	{ "Blueberry.py",NULL,    NULL,       0,            1,           0,         0,          -1 },
 	{ "Connman-gtk", NULL,    NULL,       0,            1,           0,         0,          -1 },
 	{ "Engrampa",    NULL,    NULL,       0,            1,           0,         0,          -1 },
+	{ NULL, 	  NULL,    	  "nmtui",    0,            1,           0,         0,          -1 },
 	{ "st",       NULL,       NULL,       0,            0,           1,         0,          -1 },
 	{ NULL,      NULL,     "Event Tester", 0,           0,           0,         1,          -1 }, /* xev */
 };
