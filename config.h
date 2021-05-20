27e86caf8fe9014422b580a928ff5f274e60382f
nytouu <capitaine7312@gmail.com>
2021-05-20 17:07:40 +0200

    changed colors in bar

--- config.h
+++ config.h
@@ -22,21 +22,22 @@ static const int riodraw_spawnasync = 0;        /* 0 spawns after successful sel
 static const char *fonts[]          = { "Fira Mono:size=12", "FiraMono Nerd Font:size=13" };
 static const char dmenufont[]       = "FiraMono Nerd Font Mono:size=13";
 static const char col_gray1[]       = "#1a1b26"; // default bg color
-static const char col_gray2[]       = "#414868"; // unsel win border
-static const char col_gray3[]       = "#a9b1d6"; // unsel fg color
-static const char col_gray4[]       = "#c0caf5"; // for win titles
-static const char col_gray5[]       = "#ffffff"; // sel fg
+static const char col_gray2[]       = "#24283b"; // default bg color
+static const char col_gray3[]       = "#414868"; // unsel win border
+static const char col_gray4[]       = "#a9b1d6"; // unsel fg color
+static const char col_gray5[]       = "#c0caf5"; // for win titles
+static const char col_gray6[]       = "#ffffff"; // sel fg
 static const char col_cyan[]        = "#7aa2f7"; // sel bg
 static const char col_red[]         = "#f7768e"; // urgent color
 static const char *colors[][3]      = {
 	/*               	fg         bg         border   */
-	[SchemeNorm]      = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]       = { col_gray5, col_cyan,  col_cyan  },
+	[SchemeNorm]      = { col_gray4, col_gray1, col_gray3 },
+	[SchemeSel]       = { col_gray6, col_cyan,  col_cyan  },
 	[SchemeUrg]       = { col_gray1, col_red,   col_red   }, // idk why fg and bg colors are inverted but it works so who cares
-	[SchemeTagsSel]   = { col_gray5, col_cyan,  col_cyan  }, // Tagbar left selected {text,background,not used but cannot be empty}
-    [SchemeTagsNorm]  = { col_gray3, col_gray1, col_gray2 }, // Tagbar left unselected {text,background,not used but cannot be empty}
-    [SchemeInfoSel]   = { col_gray4, col_gray1, col_cyan  }, // infobar middle  selected {text,background,not used but cannot be empty}
-    [SchemeInfoNorm]  = { col_gray2, col_gray1, col_gray2 }  // infobar middle  unselected {text,background,not used but cannot be empty}
+	[SchemeTagsSel]   = { col_gray6, col_cyan,  col_cyan  }, // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]  = { col_gray4, col_gray1, col_gray3 }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]   = { col_cyan,  col_gray2, col_cyan  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]  = { col_gray3, col_gray1, col_gray3 }  // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
 static const XPoint stickyiconbb    = {4,8};	/* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
@@ -57,7 +58,7 @@ static const Rule rules[] = {
 	{ "Blueberry.py",NULL,    NULL,       0,            1,           0,         0,          -1 },
 	{ "Connman-gtk", NULL,    NULL,       0,            1,           0,         0,          -1 },
 	{ "Engrampa",    NULL,    NULL,       0,            1,           0,         0,          -1 },
-	{ NULL, 	  NULL,    	  "nmtui",    0,            1,           0,         0,          -1 },
+	{ "st", 	  NULL,    	  "nmtui",    0,            1,           0,         0,          -1 },
 	{ "st",       NULL,       NULL,       0,            0,           1,         0,          -1 },
 	{ NULL,      NULL,     "Event Tester", 0,           0,           0,         1,          -1 }, /* xev */
 };
