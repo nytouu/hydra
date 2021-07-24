cdbe72f076d4533225e1f5c1db2da5379321b231
nytouu <capitaine7312@gmail.com>
2021-07-25 00:44:36 +0200

    update config

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int gappih    = 12;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
-static const char buttonbar[]       = " ";
+static const char buttonbar[]       = "";
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window, behaves weirdly with barpadding */
 static const int smartborders       = 0;        /* 1 means no border when there is only one window (unless floating) */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -43,7 +43,7 @@ static const char *colors[][3]      = {
 	[SchemeTagsSel]   = { col_cyan,  col_cyan,  col_cyan  }, // selected tags
     [SchemeInfo]   	  = { col_gray3, col_gray1, col_cyan  }, // focused window name text
 };
-static const unsigned int alphas[][3]      = {
+static const unsigned int alphas[][3] = {
 	/*               fg      bg        border     */
 	[SchemeNorm]      = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]       = { OPAQUE, baralpha, borderalpha },
@@ -67,9 +67,10 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   isterminal noswallow   monitor */
 	{ "discord",  NULL,       NULL,       1<<5,         0,           0,         0,          -1 },
-	{ "Firefox",  NULL,       NULL,       0,            0,           0,         -1,         -1 },
+	{ "Firefox",  NULL,       NULL,       0,            0,           0,         0,          -1 },
 	{ "Carla2-Plugin","carla-plugin",NULL,    0,        1,           0,         0,          -1 },
 	{ "Blueberry.py",NULL,    NULL,       0,            1,           0,         0,          -1 },
+	{ "Nitrogen", NULL,    	  NULL,       0,            1,           0,         0,          -1 },
 	{ "Connman-gtk", NULL,    NULL,       0,            1,           0,         0,          -1 },
 	{ "Engrampa",    NULL,    NULL,       0,            1,           0,         0,          -1 },
 	{ "st", 	  NULL,    	  "pulsemixer",0,           1,           0,         0,          -1 },
