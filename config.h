b1ce8599441f12a91fa572b1e1b39bee04383c18
nytouu <capitaine7312@gmail.com>
2021-08-16 17:35:49 +0200

    added staticstatus

--- config.h
+++ config.h
@@ -14,6 +14,7 @@ static const char buttonbar[]       = "";
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window, behaves weirdly with barpadding */
 static const int smartborders       = 0;        /* 1 means no border when there is only one window (unless floating) */
 static const int showbar            = 1;        /* 0 means no bar */
+static const int statmonval         = 1;
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int viewontag          = 1;        /* Switch view on tag switch */
 static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
--- hydra.c
+++ hydra.c
@@ -382,7 +382,7 @@ static Cur *cursor[CurLast];
 static Clr **scheme;
 static Display *dpy;
 static Drw *drw;
-static Monitor *mons, *selmon;
+static Monitor *mons, *selmon, *statmon;
 static Window root, wmcheckwin;
 static xcb_connection_t *xcon;
 
@@ -1340,7 +1340,7 @@ drawbar(Monitor *m)
 		stw = getsystraywidth();
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == statmon) { /* status is only drawn on user-defined monitor */
 		tw = statusw = m->ww - drawstatusbar(m, bh, stext);
 	}
 
@@ -3401,7 +3401,7 @@ updategeom(void)
 				else
 					mons = createmon();
 			}
-			for (i = 0, m = mons; i < nn && m; m = m->next, i++)
+			for (i = 0, m = mons; i < nn && m; m = m->next, i++){
 				if (i >= n
 				|| unique[i].x_org != m->mx || unique[i].y_org != m->my
 				|| unique[i].width != m->mw || unique[i].height != m->mh)
@@ -3414,6 +3414,9 @@ updategeom(void)
 					m->mh = m->wh = unique[i].height;
 					updatebarpos(m);
 				}
+				if (i == statmonval)
+					statmon = m;
+			}
 		} else { /* less monitors available nn < n */
 			for (i = nn; i < n; i++) {
 				for (m = mons; m && m->next; m = m->next);
@@ -3427,6 +3430,8 @@ updategeom(void)
 				}
 				if (m == selmon)
 					selmon = mons;
+				if (m == statmon)
+					statmon = mons;
 				cleanupmon(m);
 			}
 		}
@@ -3547,7 +3552,7 @@ updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
 		strcpy(stext, "hydra-"VERSION);
-	drawbar(selmon);
+	drawbar(statmon);
 	if (showsystray)
 		updatesystray();
 }
