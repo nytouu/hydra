80dae2670fbe3f1d8f2afdefa381a2d360d9f807
nytouu <capitaine7312@gmail.com>
2021-09-15 21:57:49 +0200

    lkjdsflkjsdflkj

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 #define ICONSIZE 16   /* icon size */
 #define ICONSPACING 8 /* space between icon and title */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 12;       /* horiz inner gap between windows */
