568b858c27f812e3c37f33b2b455c0e333e5f467
nytouu <capitaine7312@gmail.com>
2023-04-26 00:39:04 +0200

    restart patch

--- hydra.c
+++ hydra.c
@@ -270,6 +270,8 @@ static void setviewport(void);
 static void seturgent(Client *c, int urg);
 static void showhide(Client *c);
 static void sigchld(int unused);
+static void sighup(int unused);
+static void sigterm(int unused);
 static void sighydrablocks(const Arg *arg);
 static void spawn(const Arg *arg);
 static pid_t spawncmd(const Arg *arg);
@@ -306,6 +308,7 @@ static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void xinitvisual();
 static void xrdb(const Arg *arg);
 static void zoom(const Arg *arg);
+static void quit(const Arg *arg);
 
 static int swallow(Client *p, Client *c);
 static void unswallow(Client *c);
@@ -353,6 +356,8 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[UnmapNotify] = unmapnotify
 };
 static Atom wmatom[WMLast], netatom[NetLast];
+static int restart = 0;
+static int running = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme;
 static Display *dpy;
@@ -2355,7 +2360,7 @@ run(void)
 	XEvent ev;
 	/* main event loop */
 	XSync(dpy, False);
-	while (!XNextEvent(dpy, &ev))
+	while (running && !XNextEvent(dpy, &ev))
 		if (handler[ev.type])
 			handler[ev.type](&ev); /* call handler */
 }
@@ -2639,6 +2644,9 @@ setup(void)
 	/* clean up any zombies immediately */
 	sigchld(0);
 
+	signal(SIGHUP, sighup);
+	signal(SIGTERM, sigterm);
+
 	/* init screen */
 	screen = DefaultScreen(dpy);
 	sw = DisplayWidth(dpy, screen);
@@ -2779,6 +2787,20 @@ sighydrablocks(const Arg *arg)
 				sigqueue(hydrablockspid, SIGUSR1, sv);
 		}
 	}
+}
+
+void
+sighup(int unused)
+{
+	Arg a = {.i = 1};
+	quit(&a);
+}
+
+void
+sigterm(int unused)
+{
+	Arg a = {.i = 0};
+	quit(&a);
 }
 
 void
@@ -3532,6 +3554,13 @@ zoom(const Arg *arg)
 	pop(c);
 }
 
+void
+quit(const Arg *arg)
+{
+	if(arg->i) restart = 1;
+	running = 0;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -3556,6 +3585,7 @@ main(int argc, char *argv[])
 	scan();
 	runautostart();
 	run();
+	if(restart) execvp(argv[0], argv);
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
