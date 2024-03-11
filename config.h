39e80642af608ffb8dc6c0939f995ecc6168a0ab
nytouu <capitaine7312@gmail.com>
2024-03-11 23:47:25 +0100

    icon patch

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+#define ICONSIZE 16   /* icon size */
+#define ICONSPACING 5 /* space between icon and title */
 static const unsigned int refreshrate = 144;
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
